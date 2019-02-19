193d4f8154b5933d1404b2471718dfaaa21120f5
Piotr Gzubicki <piotr.gzubicki97@gmail.com>
2019-02-19 12:52:46 +0100

    Completed post and get styles to do

--- script.js
+++ script.js
@@ -1,25 +1,27 @@
-function callback(){
-  console.log(this.responseText);
-}
-function getCookie(cname) {
-  var name = cname + "=";
-  var decodedCookie = decodeURIComponent(document.cookie);
-  var ca = decodedCookie.split(';');
-  for(var i = 0; i <ca.length; i++) {
-    var c = ca[i];
-    while (c.charAt(0) == ' ') {
-      c = c.substring(1);
-    }
-    if (c.indexOf(name) == 0) {
-      return c.substring(name.length, c.length);
-    }
-  }
-  return "";
+$(document).ready(function() {
+  $("#formsubmit").click((event)=>{
+    event.preventDefault();
+    $.ajax({
+        headers: {
+          "centisoft_toke":"VerySecretToken1"
+        },
+        url: `http://tropson-001-site1.itempurl.com/api/developers`,
+        method:'POST',
+        data:$("#form").serialize(),
+        'success':()=>{window.location.replace("developers.html")},
+        'error':()=>{alert("ERROR")}
+    })
+  })
+});
+function redirect(id)
+{
+  window.localStorage.setItem('id', id);
+  window.location.replace('projects.html')
 }
 function getProjects(id){
   var customerName;
-  api(`customers/${getCookie('id')}/projects`).then(res=>{console.log(res);
-    api(`customers/${getCookie('id')}`).then(cust=>{
+  api(`customers/${window.localStorage.getItem('id')}/projects`).then(res=>{console.log(res);
+    api(`customers/${window.localStorage.getItem('id')}`).then(cust=>{
       customerName=cust.Name;
       document.getElementById('title').innerHTML += customerName;
       res.forEach(item=>{
@@ -35,7 +37,7 @@ function getCustomers(){
     res.forEach(item=>{
       console.log(item.Address2==null);
       var address= item.Address + " " + (item.Address2==null?"":item.Address2);
-      var row = `<tr><td>${item.Name}</td><td>${address}</td><td>${item.City}</td><td>${item.Country}</td><td>${item.Email}</td><td>${item.Phone}</td><td>${item.Zip}</td><td><button onclick="()=>{document.cookie='id=${item.Id}';window.location.replace('projects.html')}">Projects</button></td></tr>`;
+      var row = `<tr><td>${item.Name}</td><td>${address}</td><td>${item.City}</td><td>${item.Country}</td><td>${item.Email}</td><td>${item.Phone}</td><td>${item.Zip}</td><td><button onclick="redirect(${item.Id})">Projects</button></td></tr>`;
       document.getElementById('tablebody').innerHTML+=row;
     })
   }).catch(err=>{console.log(err);})
